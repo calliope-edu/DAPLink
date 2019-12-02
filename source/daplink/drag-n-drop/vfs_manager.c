9173a28a50f636e5679ab052b9284712b3b0d8dd
SzMrGfiEast <szymon.mroczek@gfieast.com>
2019-12-02 16:29:27 +0100

    Comment added.

--- source/daplink/drag-n-drop/vfs_manager.c
+++ source/daplink/drag-n-drop/vfs_manager.c
@@ -343,6 +343,10 @@ void usbd_msc_write_sect(uint32_t sector, uint8_t *buf, uint32_t num_of_sectors)
     {
         file_data_handler(sector, buf, num_of_sectors);
     }
+    else
+    {
+        // ignore processing the file transfer if it is not regarding the Root directory
+    }
 }
 
 static void sync_init(void)
